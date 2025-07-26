a1182f0a88cc304fbfc6ba9ada8bf5cbe454d6ee
cordex33 <cl.vargasparada@gmail.com>
2025-07-26 02:03:15 -0400

    Progreso de cero equalsidesofanarray

---
+++ EqualSidesOfAnArray/EqualSidesOfAnArray.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func FindEvenIndex(arr []int) int {
+  return -1
+}
+
+func main() {
+	test := FindEvenIndex([]int{1, 2, 3, 4, 3, 2, 1})
+	fmt.Println(test)
+}
