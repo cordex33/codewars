5eae36c62a826ec4387b8dd76c7b5e2f56519f14
cordex33 <cl.vargasparada@gmail.com>
2025-07-28 23:17:43 -0400

    comienzo de wordvalues

--- EqualSidesOfAnArray/EqualSidesOfAnArray.go
+++ EqualSidesOfAnArray/EqualSidesOfAnArray.go
@@ -5,12 +5,34 @@ import (
 )
 
 func FindEvenIndex(arr []int) int {
-	test := "test"
-	fmt.Println(test)
+	test := make(map[int]int)
+	test2 := make(map[int]int)
+	for i, ok := range arr {
+		switch i {
+		case 0:
+			test[i] = ok
+		case len(arr) - 1:
+
+		default:
+			test[i] = ok + test[i-1]
+		}
+
+	}
+
+	for i, x := 0, len(arr)-1; x >= 0; i, x = i+1, x-1 {
+
+		switch i {
+		case 0:
+			test2[i] = arr[x]
+		case len(arr) - 1:
+		default:
+			test2[i] = arr[x] + test2[i-1]
+		}
+	}
 	return -1
 }
 
 func main() {
-	test := FindEvenIndex([]int{1, 2, 3, 4, 3, 2, 1})
+	test := FindEvenIndex([]int{1, 100, 50, -51, 1, 1})
 	fmt.Println(test)
 }
