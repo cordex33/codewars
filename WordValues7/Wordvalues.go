c22895d437baa2eb4e4113311b34ee28c3c3d6db
cordex33 <cl.vargasparada@gmail.com>
2025-07-28 23:41:04 -0400

    Funciona WordValues7

--- WordValues7/Wordvalues.go
+++ WordValues7/Wordvalues.go
@@ -4,12 +4,29 @@ import (
 	"fmt"
 )
 
-func NameValues(my_list []string) []int {
+func NameValue(my_list []string) []int {
 
-	return []int{}
+	total := 0
+	xd := []int{}
+	for multi, x := range my_list {
+		for _, xd := range x {
+			if string(xd) != " " {
+				total = total + int(xd) - 96
+			}
+		}
+		if multi > 0 {
+			xd = append(xd, total*(multi+1))
+		} else {
+			xd = append(xd, total)
+		}
+
+		total = 0
+	}
+
+	return xd
 }
 
 func main() {
-	test := NameValues([]string{"codewars", "abc", "xyz"})
+	test := NameValue([]string{"abc", "abc", "abc", "abc"})
 	fmt.Println(test)
 }
