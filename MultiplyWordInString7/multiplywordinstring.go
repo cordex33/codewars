ff4b62b986aaaa5f5ace6b7d2df46216145e1b49
cordex33 <cl.vargasparada@gmail.com>
2025-07-29 23:30:13 -0400

    terminado multiplywordinstring

--- MultiplyWordInString7/multiplywordinstring.go
+++ MultiplyWordInString7/multiplywordinstring.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func ModifyMultiply(str string, loc, num int) string {
-	return ""
+	xd := strings.Split(str, " ")
+	test := ""
+	if num == 0 {
+		return xd[loc]
+	}
+	test = strings.Repeat(xd[loc]+"-", num)
+	test = test[:len(test)-1]
+	return test
 }
 
 func main() {
-	test := ModifyMultiply("This is a stringvvvv", 3, 5)
+	test := ModifyMultiply("EfgHOkfRJAEdldPQ wieWg UOJdDCjFThoHEMCDZTK aGuRYSBzeKzuzteuQ", 3, 0)
 	fmt.Println(test)
 }
