09ba4d00ddb9e6adc680d715fb3bc38efa01bf25
cordex33 <cl.vargasparada@gmail.com>
2025-07-29 23:01:36 -0400

    comienzo multiplywordinstring

---
+++ MultiplyWordInString7/multiplywordinstring.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func ModifyMultiply(str string, loc, num int) string {
+	return ""
+}
+
+func main() {
+	test := ModifyMultiply("This is a stringvvvv", 3, 5)
+	fmt.Println(test)
+}
