b8a72525e4f23d7766b460ea659ef008c6734ec3
cordex33 <cl.vargasparada@gmail.com>
2025-07-30 23:58:30 -0400

    terminado a rule of divisibility by 13

--- ARuleOfDivisibilityBy13/6/aruleofdivisibilityby13.go
+++ ARuleOfDivisibilityBy13/6/aruleofdivisibilityby13.go
@@ -2,13 +2,39 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
+func reverse(str string) (result string) {
+	for _, v := range str {
+		result = string(v) + result
+	}
+	return
+}
+
 func Thirt(n int) int {
-	return 1
+	list := []int{1, 10, 9, 12, 3, 4}
+	num := reverse(strconv.Itoa(n))
+	devolver := 0
+	usado := 0
+	for x := 0; len(num) > 2; x++ {
+		devolver = 0
+		usado = 0
+		for _, xd := range num {
+			a, _ := strconv.Atoi(string(xd))
+			devolver = devolver + (a * list[usado])
+			usado++
+			if usado == 6 {
+				usado = 0
+			}
+		}
+		num = reverse(strconv.Itoa(devolver))
+	}
+
+	return devolver
 }
 
 func main() {
-	test := Thirt(8529)
+	test := Thirt(1234567)
 	fmt.Println(test)
 }
