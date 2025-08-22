e545001551703220c6a300016fbad3e9115a3d89
cordex33 <cl.vargasparada@gmail.com>
2025-08-22 00:31:55 -0400

    inicio cata encript this nivel6!

---
+++ go/Encrypthis6/encrypthis.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func EncryptThis(text string) string {
+	// Implement me! :)
+
+	return ""
+}
+
+func main() {
+	test := EncryptThis("hello world")
+	fmt.Println(test)
+}
